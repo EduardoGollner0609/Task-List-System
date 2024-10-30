c5e7e98431fdc9b400397172c6a380142b8efed5
EduGollner0609 <dudugollner@gmail.com>
2024-10-30 07:28:36 -0300

    Criando método para atualizar tarefa.

--- BackEnd/src/main/java/com/eduardo/tasklistsystem/services/TaskService.java
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/services/TaskService.java
@@ -3,6 +3,7 @@ package com.eduardo.tasklistsystem.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.eduardo.tasklistsystem.entities.Task;
 import com.eduardo.tasklistsystem.repositories.TaskRepository;
 import com.eduardo.tasklistsystem.services.exceptions.ResourceNotFoundException;
 
@@ -19,4 +20,15 @@ public class TaskService {
 		repository.deleteById(id);
 	}
 
+	public Task update(Long id, Task task) {
+		Task obj = repository.findById(id).orElseThrow(() -> new RuntimeException("Tarefa não encontrada"));
+		updateData(obj, task);
+		return repository.save(obj);
+	}
+
+	private void updateData(Task obj, Task task) {
+		obj.setName(task.getName());
+		obj.setCusto(task.getCusto());
+		obj.setLimitDate(task.getLimitDate());
+	}
 }
