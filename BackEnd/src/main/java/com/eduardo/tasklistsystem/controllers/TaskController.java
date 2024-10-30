76e80a3b8ab9cd3bdc47e23dd91e1a07bfc00ed6
EduGollner0609 <dudugollner@gmail.com>
2024-10-30 08:04:01 -0300

    Criando camada Controller.

---
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/controllers/TaskController.java
@@ -0,0 +1,17 @@
+package com.eduardo.tasklistsystem.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardo.tasklistsystem.services.TaskService;
+
+@RestController
+@RequestMapping(value = "/tasks")
+public class TaskController {
+
+	@Autowired
+	private TaskService service;
+	
+	
+}
