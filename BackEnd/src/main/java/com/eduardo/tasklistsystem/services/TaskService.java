c43b0641ff5906dd5d5c6842ace58cf7fc29de76
EduGollner0609 <dudugollner@gmail.com>
2024-10-30 06:35:22 -0300

    Criando método para deletar tarefa.

--- BackEnd/src/main/java/com/eduardo/tasklistsystem/services/TaskService.java
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/services/TaskService.java
@@ -4,12 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eduardo.tasklistsystem.repositories.TaskRepository;
+import com.eduardo.tasklistsystem.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class TaskService {
 
 	@Autowired
 	private TaskRepository repository;
-	
-	
+
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Tarefa não encontrada.");
+		}
+		repository.deleteById(id);
+	}
+
 }
