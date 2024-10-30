2ca7464ea9d4b7aba0393b9f3081922d7c0baf5e
EduGollner0609 <dudugollner@gmail.com>
2024-10-30 08:10:45 -0300

    Implementando lógica.

--- BackEnd/src/main/java/com/eduardo/tasklistsystem/services/TaskService.java
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/services/TaskService.java
@@ -1,5 +1,7 @@
 package com.eduardo.tasklistsystem.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,10 @@ public class TaskService {
 
 	@Autowired
 	private TaskRepository repository;
+
+	public List<TaskDTO> findAll() {
+		return repository.findAllByOrderByOrderApresentationAsc().stream().map(x -> new TaskDTO(x)).toList();
+	}
 
 	public void delete(Long id) {
 		if (!repository.existsById(id)) {
