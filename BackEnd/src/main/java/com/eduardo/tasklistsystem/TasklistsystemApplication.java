128a7d56e4eb47c396a3a4c19b8e98e891fd7f7b
EduGollner0609 <dudugollner@gmail.com>
2024-10-30 06:12:52 -0300

    first commit.

---
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/TasklistsystemApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.tasklistsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklistsystemApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/eduardo/tasklistsystem/TasklistsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.tasklistsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
