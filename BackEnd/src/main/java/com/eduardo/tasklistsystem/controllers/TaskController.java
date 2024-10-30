25ad07152dcd4c669e2af882ce45d71ed99cf696
EduGollner0609 <dudugollner@gmail.com>
2024-10-30 08:22:54 -0300

    Criando método na camada Controller para atualizar,.

--- BackEnd/src/main/java/com/eduardo/tasklistsystem/controllers/TaskController.java
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/controllers/TaskController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,10 @@ public class TaskController {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<TaskDTO> update(@PathVariable Long id, @RequestBody TaskDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
 }
