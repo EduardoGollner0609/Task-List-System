d9d929eab29208cb4d1741aeffb823a80ca1a1af
EduGollner0609 <dudugollner@gmail.com>
2024-10-30 06:27:31 -0300

    Mapeando entidade Task com JPA.

--- BackEnd/src/main/java/com/eduardo/tasklistsystem/entities/Task.java
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/entities/Task.java
@@ -2,8 +2,18 @@ package com.eduardo.tasklistsystem.entities;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_task")
 public class Task {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double custo;
