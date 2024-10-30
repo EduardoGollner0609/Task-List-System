b6a20660efce41cadd6cf7f7e133301d403851bc
EduGollner0609 <dudugollner@gmail.com>
2024-10-30 08:12:49 -0300

    Criando método para retornar todas as tarefas no Controller.

--- BackEnd/src/main/java/com/eduardo/tasklistsystem/controllers/TaskController.java
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/controllers/TaskController.java
@@ -1,9 +1,13 @@
 package com.eduardo.tasklistsystem.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eduardo.tasklistsystem.dto.TaskDTO;
 import com.eduardo.tasklistsystem.services.TaskService;
 
 @RestController
@@ -12,6 +16,9 @@ public class TaskController {
 
 	@Autowired
 	private TaskService service;
-	
 
+	public ResponseEntity<List<TaskDTO>> findAll() {
+		List<TaskDTO> tasks = service.findAll();
+		return ResponseEntity.ok().body(tasks);
+	}
 }
