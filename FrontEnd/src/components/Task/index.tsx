f159fde05215c77ff26c1b3f5fefaf5f6ae2369e
EduGollner0609 <dudugollner@gmail.com>
2024-10-31 08:46:52 -0300

    Adicionando HTML e CSS ao componente Task.

--- FrontEnd/src/components/Task/index.tsx
+++ FrontEnd/src/components/Task/index.tsx
@@ -3,7 +3,14 @@ import "./styles.css";
 export default function Task() {
   return (
     <div className="card-task">
-    <h1>Tarefa</h1>
+      <p>ID: 10</p>
+      <p>Tarefa vem aqui e se for maior como ela se enquadra aqui</p>
+      <p>Custo: R$20.00</p>
+      <p>Data limite: 20/07/2024</p>
+      <div className="card-task-icons">
+        <ion-icon name="arrow-up-outline"></ion-icon>
+        <ion-icon name="arrow-down-outline"></ion-icon>
+      </div>
     </div>
   );
 }
