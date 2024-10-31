64ae7c1bc725979e2256a7e95f434baf7c970b81
EduGollner0609 <dudugollner@gmail.com>
2024-10-31 11:30:57 -0300

    Criando função de aparecer formulario.

--- FrontEnd/src/routes/ListTasks/index.tsx
+++ FrontEnd/src/routes/ListTasks/index.tsx
@@ -6,21 +6,35 @@ import CardCreateTask from "../../components/CardCreateTask";
 export default function ListTasks() {
   const { data } = useTaskData();
 
+  function showModalCreate() {
+    const cardCreateTask = document.querySelector(".card-create-task");
+    if (cardCreateTask != null) {
+      cardCreateTask.classList.add("active-create-task");
+    }
+  }
+  
   return (
     <main>
       <h1 id="title-home-page">Lista de Tarefas</h1>
       <section className="section-list-tasks">
         <CardCreateTask />
         <div className="list-tasks">
-          {data?.map((taskData: { id: number; name: string; cost: number; limitDate: Date; }) => (
-            <Task
-              id={taskData.id}
-              name={taskData.name}
-              cost={taskData.cost}
-              limitDate={taskData.limitDate}
-            />
-          ))}
-          <div className="function-add-task">
+          {data?.map(
+            (taskData: {
+              id: number;
+              name: string;
+              cost: number;
+              limitDate: Date;
+            }) => (
+              <Task
+                id={taskData.id}
+                name={taskData.name}
+                cost={taskData.cost}
+                limitDate={taskData.limitDate}
+              />
+            )
+          )}
+          <div className="function-add-task" onClick={showModalCreate}>
             <ion-icon name="add-circle-outline"></ion-icon>
             <p>Adicionar nova tarefa</p>
           </div>
