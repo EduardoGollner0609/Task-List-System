7466f411c69e6ee572aadd8ea2cb234d3e0d18ac
EduGollner0609 <dudugollner@gmail.com>
2024-11-03 20:03:58 -0300

    Adicionando variavies de ambiente para armazenar valor do cors.

--- BackEnd/src/main/java/com/eduardo/tasklistsystem/configs/WebConfig.java
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/configs/WebConfig.java
@@ -1,5 +1,6 @@
 package com.eduardo.tasklistsystem.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	@Value("${cors.origins}")
+	private String corsOrigins;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:5173")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true);
+		registry.addMapping("/**").allowedOrigins(corsOrigins).allowedMethods("GET", "POST", "PUT", "DELETE",
+				"OPTIONS");
 	}
 }
