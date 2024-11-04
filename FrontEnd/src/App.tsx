6ea6812e043c47a4db6ccae5d51cd24b981782ed
EduGollner0609 <dudugollner@gmail.com>
2024-11-04 15:52:13 -0300

    tentando outra forma de deploy.

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import HomePage from './routes/HomePage'
-import ListTasks from './routes/ListTasks'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./routes/HomePage";
+import ListTasks from "./routes/ListTasks";
 
 function App() {
   return (
-<BrowserRouter>
-<Routes>
-  <Route path="/" element={<HomePage />}></Route>
-  <Route path="/list-tasks" element={<ListTasks />}></Route>
-</Routes>
-</BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/list-tasks" element={<ListTasks />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
