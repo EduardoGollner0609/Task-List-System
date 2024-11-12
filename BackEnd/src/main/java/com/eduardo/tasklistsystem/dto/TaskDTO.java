eb0f9cc36f7e8798d99c0ac30a0b396d0a58582b
EduGollner0609 <dudugollner@gmail.com>
2024-11-12 00:09:47 -0300

    Adicionando validações para tornar os campos de custo, data limite e horario limite como obrigatorios.

--- BackEnd/src/main/java/com/eduardo/tasklistsystem/dto/TaskDTO.java
+++ BackEnd/src/main/java/com/eduardo/tasklistsystem/dto/TaskDTO.java
@@ -5,17 +5,22 @@ import java.time.LocalTime;
 
 import com.eduardo.tasklistsystem.entities.Task;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class TaskDTO {
 
 	private Long id;
 	@NotBlank(message = "O nome não pode estar vazio")
-	@Size(min = 5, max=50, message = "O nome deve ter pelo entre 5 a 50 caracteres")
+	@Size(min = 5, max = 50, message = "O nome deve ter pelo entre 5 a 50 caracteres")
 	private String name;
+	@Min(value = 1, message = "O custo não pode ser 0 ou negativo.")
 	private Double cost;
+	@NotNull(message = "O prazo não pode estar vazio")
 	private LocalDate limitDate;
+	@NotNull(message = "O horário não pode estar vazio")
 	private LocalTime limitTime;
 
 	public TaskDTO() {
